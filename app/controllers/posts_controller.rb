dba9bd0de66abf64c6c2104738bd0ee4e29cc3c6
Edward Young <edward.young3@btinternet.com>
2016-03-05 19:58:56 +0000

    Adding the first post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
   def hello_world
   end
+  def fundermentals
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/hello_world'
+  get 'posts/fundermentals'
 
   get 'home/index'
 
